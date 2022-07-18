035378737526debc8aa199a86ca331f31aa65524
IkomaChan81 <victor.icoma@unesp.br>
2022-07-18 11:04:06 -0300

    Aula 5 regras de negocios de Aluno com uso de privados parte 2

--- aula5/Aluno.java
+++ aula5/Aluno.java
@@ -4,18 +4,57 @@ public class Aluno extends Pessoa {
     private String curso;
     private double[] notas;
 
-    //#region Encapsulamento [Getters e Setters]
+    // #region Encapsulamento [Getters e Setters]
     public String getCurso() {
         return curso;
     }
+
     public void setCurso(String curso) {
         this.curso = curso;
     }
+
     public double[] getNotas() {
         return notas;
     }
+
     public void setNotas(double[] notas) {
         this.notas = notas;
     }
-    //#endregion
+    // #endregion
+
+    // #region Regras de Negócios
+    private double calculaMedia() {
+        double media = 0;
+        for (int i = 0; i < this.notas.length; i++) {
+            media += notas[i];
+        }
+        return (media / notas.length);
+    }
+
+    private boolean verificaAprovacao(double media) {
+        if (media >= 7) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String montaBoletim() {
+        String boletim = ("Nome do Aluno: " + super.getNome() + System.lineSeparator());
+        if (this.verificaAprovacao(this.calculaMedia()) == false) {
+            boletim += (System.lineSeparator()
+                    + "Devido a política da academia, o resultado está disponivel on-line");
+        } else {
+            boletim += ("CPF: " + super.getCpf()
+                    + System.lineSeparator()
+                    + System.lineSeparator());
+            for (int i = 0; i < this.notas.length; i++) {
+                boletim += (" avaliacao: " + this.notas[i] + " | " + System.lineSeparator());
+            }
+            boletim += ("Resultado: Aprovado." + System.lineSeparator());
+            boletim += ("Media final: " + this.calculaMedia() + System.lineSeparator());
+        }
+        return boletim;
+    }
+    // #endregion
 }
--- aula5/Professor.java
+++ aula5/Professor.java
@@ -1,5 +1,24 @@
 package aula5;
 
 public class Professor extends Pessoa{
+    private double salario;
+    private String nomeCurso;
 
+    //#region Encapsulamento [Getters e Setters]
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+    //#endregion
+
+    //#region Regras de Netgócios
+    
 }
