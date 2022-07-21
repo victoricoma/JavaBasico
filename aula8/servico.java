1bebd37080b332b12f75cc16c96b4539917f0bcf
IkomaChan81 <victor.icoma@unesp.br>
2022-07-21 14:02:05 -0300

    Aula 8 parte 2 chamando na classe principal classes enumeradoras e construtores compostos

--- aula8/servico.java
+++ aula8/servico.java
@@ -2,6 +2,17 @@ package aula8;
 
 public class servico {
     public static void main(String[] args) {
+
+        Pessoa pFisica = new Pessoa();
         
+        pFisica.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CPF"));
+        pFisica.setNumeroDocumento(pFisica.getTipoDocumento().gerarNumeroTeste());
+        System.out.println(pFisica);
+
+        Pessoa pJuridica = new Pessoa();
+
+        pJuridica.setTipoDocumento(Enum.valueOf(TipoDocumento.class, "CNPJ"));
+        pJuridica.setNumeroDocumento(pJuridica.getTipoDocumento().gerarNumeroTeste());
+        System.out.println(pJuridica);
     }
 }
